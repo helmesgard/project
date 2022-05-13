bbdf0cd4f9e128863fea12a3290c4b3fb450d2b3
helmesgard <93432526+helmesgard@users.noreply.github.com>
2022-05-13 23:41:28 +0800

    Update Tetris.py

--- Tetris.py
+++ Tetris.py
@@ -29,11 +29,11 @@ field = [[0 for i in range(W)] for j in range(H)]
 
 anim_count, anim_speed, anim_limit = 0, 60, 2000
 
-bg = pygame.image.load('img/bg.jpg').convert()
-game_bg = pygame.image.load('img/bg2.jpg').convert()
+bg = pygame.image.load('bg.jpg').convert()
+game_bg = pygame.image.load('bg2.jpg').convert()
 
 main_font = pygame.font.Font('font/font.ttf', 65)
-font = pygame.font.Font('font/font.ttf', 45)
+font = pygame.font.Font('font.ttf', 45)
 
 title_tetris = main_font.render('TETRIS', True, pygame.Color('darkorange'))
 title_score = font.render('score:', True, pygame.Color('green'))
